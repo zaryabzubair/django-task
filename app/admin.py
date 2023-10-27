94fa6b471edf27c9c91fd8c8e53e532ca06cf002
zaryab <zaryabzubair.m@gmail.com>
2023-10-28 02:35:24 +0500

    add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Company, Employee, EmployeeHistory
 
-# Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Employee)
+admin.site.register(EmployeeHistory)
